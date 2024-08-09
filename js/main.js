39888c989e3d82d31eec402e1bf7d9b60cd24b92
Shepherd-Moahloli <shepherdmoahloli122@gmail.com>
2024-08-09 20:30:25 +0200

    Lets gooo

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var slides = document.querySelectorAll('#slider .slide');
+var currentSlide = 0;
+var isAnimating = false;
+
+function changeSlide() {
+    if (isAnimating) return;
+    isAnimating = true;
+
+    // Move the current slide out
+    slides[currentSlide].style.transform = 'translateX(-100%)';
+
+    // Move to the next slide
+    var nextSlide = (currentSlide + 1) % slides.length;
+    slides[nextSlide].style.transform = 'translateX(0)';
+    slides[nextSlide].style.zIndex = 2;
+
+    // After the animation is complete, reset the position of the previous slide
+    setTimeout(() => {
+        slides[currentSlide].style.transform = 'translateX(100%)';
+        slides[currentSlide].style.zIndex = 1;
+        currentSlide = nextSlide;
+        isAnimating = false;
+    }, 1000); // delay equal to transition duration
+}
+
+// Change slide every 3 seconds
+setInterval(changeSlide, 3000);
+
+
+document.querySelectorAll('.my-svg').forEach(function(img) {
+    img.parentNode.addEventListener('mouseover', function() {
+        img.src = 'images/circle-right-regular-red.svg'; // path to the red SVG
+    });
+    img.parentNode.addEventListener('mouseout', function() {
+        img.src = 'images/circle-right-regular.svg'; // path to the original SVG
+    });
+});
